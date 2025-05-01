70163a5cd303bc71749b6c0322cc0127ff3db77a
kimiti <kimitikevin@gmail.com>
2025-05-01 08:00:00 +0300

    add intro screen

--- app/src/main/java/com/example/onlineshop/ui/screens/IntroScreen.kt
+++ app/src/main/java/com/example/onlineshop/ui/screens/IntroScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -12,6 +13,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,7 +30,11 @@ fun IntroScreen(){
     ) {
         Image(
             painter = painterResource(R.drawable.intro_pic),
-            contentDescription = null
+            contentDescription = null,
+            modifier = Modifier
+                .padding(top = 48.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Fit
         )
         Spacer(modifier = Modifier.height(32.dp))
     }
