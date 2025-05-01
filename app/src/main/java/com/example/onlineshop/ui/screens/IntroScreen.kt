103822bc18fc6d0a86e048b31cf6db137f509474
kimiti <kimitikevin@gmail.com>
2025-05-01 08:10:00 +0300

    add intro screen

--- app/src/main/java/com/example/onlineshop/ui/screens/IntroScreen.kt
+++ app/src/main/java/com/example/onlineshop/ui/screens/IntroScreen.kt
@@ -10,13 +10,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.onlineshop.R
 
 @Preview
@@ -37,6 +42,12 @@ fun IntroScreen(){
             contentScale = ContentScale.Fit
         )
         Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            text = stringResource(R.string.title),
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
     }
 
 }
