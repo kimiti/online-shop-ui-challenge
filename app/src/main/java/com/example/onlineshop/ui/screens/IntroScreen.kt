4a400fec93acba75dec74637dd661684dea53217
kimiti <kimitikevin@gmail.com>
2025-05-02 08:30:00 +0300

    add intro screen

--- app/src/main/java/com/example/onlineshop/ui/screens/IntroScreen.kt
+++ app/src/main/java/com/example/onlineshop/ui/screens/IntroScreen.kt
@@ -9,12 +9,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -26,12 +31,14 @@ import com.example.onlineshop.R
 
 @Preview
 @Composable
-fun IntroScreen(){
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.White)
-        .verticalScroll(rememberScrollState())
-        .padding(16.dp)
+fun IntroScreen(onClick: () -> Unit = {}) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(R.drawable.intro_pic),
@@ -54,6 +61,30 @@ fun IntroScreen(){
             color = Color.DarkGray,
             lineHeight = 24.sp,
             textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .padding(horizontal = 16.dp)
+        )
+        Button(
+            onClick = { onClick() },
+            modifier = Modifier
+                .padding(horizontal = 32.dp, vertical = 16.dp)
+                .fillMaxWidth()
+                .height(50.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(R.color.blue)),
+            shape = RoundedCornerShape(10.dp)
+        )
+        {
+            Text(
+                text = stringResource(R.string.start),
+                color = Color.White,
+                fontSize = 18.sp
+            )
+        }
+        Text(
+            text = stringResource(R.string.signIn),
+            textAlign = TextAlign.Center,
+            fontSize = 18.sp,
             modifier = Modifier
                 .padding(top = 16.dp)
                 .padding(horizontal = 16.dp)
