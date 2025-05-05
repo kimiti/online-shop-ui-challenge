4227006291c54677439db7b7d7bc9448c1a890c8
kimiti <kimitikevin@gmail.com>
2025-05-05 08:00:00 +0300

    add model

--- app/src/main/java/com/example/onlineshop/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/onlineshop/viewModel/MainViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.onlineshop.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.example.onlineshop.repository.MainRepository
 
 class MainViewModel: ViewModel() {
+    private val repository = MainRepository()
+    
 }
