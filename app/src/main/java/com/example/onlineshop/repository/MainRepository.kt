be6a5ad2164d8465d26a984ada3f97f02ec7b8b5
kimiti <kimitikevin@gmail.com>
2025-05-05 08:40:00 +0300

    add model

--- app/src/main/java/com/example/onlineshop/repository/MainRepository.kt
+++ app/src/main/java/com/example/onlineshop/repository/MainRepository.kt
@@ -1,4 +1,15 @@
 package com.example.onlineshop.repository
 
+import com.example.onlineshop.R
+import com.example.onlineshop.model.SliderModel
+
 class MainRepository {
+    val banners = mutableListOf(
+        SliderModel(
+            url = R.drawable.banner1
+        ),
+        SliderModel(
+            url = R.drawable.banner2
+        )
+    )
 }
