48b64de9eeefcf44904c11864639f10e0d3d5698
kimiti <kimitikevin@gmail.com>
2025-05-24 08:20:00 +0300

    add model

--- app/src/main/java/com/example/onlineshop/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/onlineshop/ui/screens/MainScreen.kt
@@ -1,9 +1,26 @@
 package com.example.onlineshop.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.onlineshop.viewModel.MainViewModel
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier){
+fun MainScreen(modifier: Modifier = Modifier) {
+    val viewModel = remember { MainViewModel() }
+
+    ConstraintLayout(modifier = Modifier.background(Color.White)) {
+        val (scrollList, bottomMenu) = createRefs()
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+        ) { }
+    }
 
 }
