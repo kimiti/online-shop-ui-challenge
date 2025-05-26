8e6e3f647e051d03db326822554d63b6e901b218
kimiti <kimitikevin@gmail.com>
2025-05-26 08:00:00 +0300

    add model

--- app/src/main/java/com/example/onlineshop/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/onlineshop/ui/screens/MainScreen.kt
@@ -1,15 +1,32 @@
 package com.example.onlineshop.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.onlineshop.R
 import com.example.onlineshop.viewModel.MainViewModel
 
+@Preview
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     val viewModel = remember { MainViewModel() }
@@ -25,7 +42,38 @@ fun MainScreen(modifier: Modifier = Modifier) {
                     bottom.linkTo(anchor = parent.bottom)
                     end.linkTo(anchor = parent.start)
                 }
-        ) { }
+        ) {
+            item {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 48.dp, start = 16.dp, end = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column {
+                        Text(text = "Welcome Back", color = Color.Black)
+                        Text(
+                            text = "Jackie",
+                            color = Color.Black,
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                    Row {
+                        Image(
+                            painter = painterResource(id = R.drawable.fav_icon),
+                            contentDescription = null
+                        )
+                        Spacer(modifier = Modifier.width(width = 16.dp))
+                        Image(
+                            painter = painterResource(id = R.drawable.search_icon),
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
+        }
     }
 
 }
